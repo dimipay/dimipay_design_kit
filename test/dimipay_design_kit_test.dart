f3052dcbc76da04fabd3d70260080190031eb7ca
hayward2007 <hayward2007@naver.com>
2024-01-18 01:46:57 +0900

    Modified description

--- test/dimipay_design_kit_test.dart
+++ test/dimipay_design_kit_test.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dimipay_design_kit/dimipay_design_kit.dart';
+// import 'package:dimipay_design_kit/dimipay_design_kit.dart';
 
 void main() {
   test('adds one to input values', () {
-    final text = Text("Hello Dimigo Students!", style: DPTypography.title());
+    // final text = Text("Hello Dimigo Students!", style: DPTypography.title());
     // var a = DPColors.grayscale100;
   });
 }
