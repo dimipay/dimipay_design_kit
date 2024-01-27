0c9e5fd3fde8e08b90a198b699d1b1434fd96dba
Inhwa Jang <superjang3357@naver.com>
2024-01-27 13:45:22 +0900

    v0.0.10 fix DPTypography.readable fontsize remove get dependency

--- lib/utils/dimipay_typography.dart
+++ lib/utils/dimipay_typography.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dimipay_design_kit/dimipay_design_kit.dart';
 
-TextStyle _style(FontWeight weight, double size, double height, Color color,
-        {bool underlined = false, double spacing = 0}) =>
-    TextStyle(
+TextStyle _style(FontWeight weight, double size, double height, Color color, {bool underlined = false, double spacing = 0}) => TextStyle(
       color: color,
       height: height / size,
       fontSize: size,
@@ -30,60 +28,43 @@ class _SUITv1 {
 }
 
 class _POS {
-  TextStyle itemTitle({Color color = _defaultColor}) =>
-      _style(_weight.semiBold, 24, 0, color);
+  TextStyle itemTitle({Color color = _defaultColor}) => _style(_weight.semiBold, 24, 0, color);
 
-  TextStyle itemDescription({Color color = _defaultColor}) =>
-      _style(_weight.medium, 24, 0, color);
+  TextStyle itemDescription({Color color = _defaultColor}) => _style(_weight.medium, 24, 0, color);
 
-  TextStyle title({Color color = _defaultColor}) =>
-      _style(_weight.semiBold, 36, 0, color, spacing: -0.72);
+  TextStyle title({Color color = _defaultColor}) => _style(_weight.semiBold, 36, 0, color, spacing: -0.72);
 
-  TextStyle underlined({Color color = _defaultColor}) =>
-      _style(_weight.regular, 24, 0, color, underlined: true);
+  TextStyle underlined({Color color = _defaultColor}) => _style(_weight.regular, 24, 0, color, underlined: true);
 }
 
 abstract class DPTypography {
   static final weight = _weight;
 
-  static TextStyle title({Color color = _defaultColor}) =>
-      _style(_weight.semiBold, 28, 36, color);
+  static TextStyle title({Color color = _defaultColor}) => _style(_weight.semiBold, 28, 36, color);
 
-  static TextStyle header1({Color color = _defaultColor}) =>
-      _style(_weight.bold, 24, 32, color);
+  static TextStyle header1({Color color = _defaultColor}) => _style(_weight.bold, 24, 32, color);
 
-  static TextStyle header2({Color color = _defaultColor}) =>
-      _style(_weight.semiBold, 20, 26, color);
+  static TextStyle header2({Color color = _defaultColor}) => _style(_weight.semiBold, 20, 26, color);
 
-  static TextStyle itemTitle({Color color = _defaultColor}) =>
-      _style(_weight.semiBold, 16, 22, color);
+  static TextStyle itemTitle({Color color = _defaultColor}) => _style(_weight.semiBold, 16, 22, color);
 
-  static TextStyle itemDescription({Color color = _defaultColor}) =>
-      _style(_weight.medium, 14, 20, color);
+  static TextStyle itemDescription({Color color = _defaultColor}) => _style(_weight.medium, 14, 20, color);
 
-  static TextStyle description({Color color = _defaultColor}) =>
-      _style(_weight.semiBold, 16, 22, color);
+  static TextStyle description({Color color = _defaultColor}) => _style(_weight.semiBold, 16, 22, color);
 
-  static TextStyle readable({Color color = _defaultColor}) =>
-      _style(_weight.semiBold, 20, 20, color);
+  static TextStyle readable({Color color = _defaultColor}) => _style(_weight.semiBold, 14, 20, color);
 
-  static TextStyle token({Color color = _defaultColor}) =>
-      _style(_weight.regular, 12, 18, color);
+  static TextStyle token({Color color = _defaultColor}) => _style(_weight.regular, 12, 18, color);
 
-  static TextStyle hint({Color color = _defaultColor}) =>
-      _style(_weight.regular, 10, 16, color);
+  static TextStyle hint({Color color = _defaultColor}) => _style(_weight.regular, 10, 16, color);
 
-  static TextStyle paragraph1({Color color = _defaultColor}) =>
-      _style(_weight.regular, 16, 16 * 1.6, color);
+  static TextStyle paragraph1({Color color = _defaultColor}) => _style(_weight.regular, 16, 16 * 1.6, color);
 
-  static TextStyle paragraph2({Color color = _defaultColor}) =>
-      _style(_weight.regular, 14, 14 * 1.6, color);
+  static TextStyle paragraph2({Color color = _defaultColor}) => _style(_weight.regular, 14, 14 * 1.6, color);
 
-  static TextStyle paragraph1Underlined({Color color = _defaultColor}) =>
-      _style(_weight.regular, 16, 16 * 1.6, underlined: true, color);
+  static TextStyle paragraph1Underlined({Color color = _defaultColor}) => _style(_weight.regular, 16, 16 * 1.6, underlined: true, color);
 
-  static TextStyle paragraph2Underlined({Color color = _defaultColor}) =>
-      _style(_weight.regular, 14, 14 * 1.6, underlined: true, color);
+  static TextStyle paragraph2Underlined({Color color = _defaultColor}) => _style(_weight.regular, 14, 14 * 1.6, underlined: true, color);
 
   static final pos = _POS();
 }
