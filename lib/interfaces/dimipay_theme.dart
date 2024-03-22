b7013dd9701ff5e576870de2c54f7aa291f5b4d0
Inhwa Jang <superjang3357@naver.com>
2024-03-22 21:56:21 +0900

    remove widgets

--- lib/interfaces/dimipay_theme.dart
+++ lib/interfaces/dimipay_theme.dart
@@ -1,4 +1,5 @@
-import 'package:dimipay_design_kit/dimipay_design_kit.dart';
+import 'package:dimipay_design_kit/interfaces/dimipay_colors.dart';
+import 'package:dimipay_design_kit/interfaces/dimipay_typography.dart';
 
 class DPTheme {
   IDPColors colors;
