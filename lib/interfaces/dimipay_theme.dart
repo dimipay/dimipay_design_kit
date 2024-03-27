1e461fd57ed7112a21c97366095d9cedcc9c53a9
Inhwa Jang <superjang3357@naver.com>
2024-03-28 00:31:02 +0900

    apply ThemeExtension

--- lib/interfaces/dimipay_theme.dart
+++ lib/interfaces/dimipay_theme.dart
@@ -2,7 +2,7 @@ import 'package:dimipay_design_kit/interfaces/dimipay_colors.dart';
 import 'package:dimipay_design_kit/interfaces/dimipay_typography.dart';
 
 class DPTheme {
-  IDPColors colors;
-  IDPTypography textStyle;
+  final DPColors colors;
+  final IDPTypography textStyle;
   DPTheme({required this.colors, required this.textStyle});
 }
--- lib/theme/dark.dart
+++ lib/theme/dark.dart
@@ -3,31 +3,22 @@ import 'package:dimipay_design_kit/interfaces/dimipay_theme.dart';
 import 'package:dimipay_design_kit/interfaces/dimipay_typography.dart';
 import 'package:flutter/material.dart';
 
-class DPDarkThemeColors implements IDPColors {
-  @override
-  Color grayscale100 = const Color(0xFF222525);
-  @override
-  Color grayscale200 = const Color(0xFF1C1F1F);
-  @override
-  Color grayscale300 = const Color(0xFF2C3030);
-  @override
-  Color grayscale400 = const Color(0xFF3B3F40);
-  @override
-  Color grayscale500 = const Color(0xFF808989);
-  @override
-  Color grayscale600 = const Color(0xFFB4B9B9);
-  @override
-  Color grayscale700 = const Color(0xFFDADDDD);
-  @override
-  Color grayscale800 = const Color(0xFFEAEBEB);
-  @override
-  Color grayscale900 = const Color(0xFFF4F5F5);
-  @override
-  Color grayscale1000 = const Color(0xFFFDFEFE);
-  @override
-  Color primaryBrand = const Color(0xFF2EA4AB);
-  @override
-  Color primaryNegative = const Color(0xFFD76263);
+class DPDarkThemeColors extends DPColors {
+  DPDarkThemeColors()
+      : super(
+          grayscale100: const Color(0xFF222525),
+          grayscale200: const Color(0xFF1C1F1F),
+          grayscale300: const Color(0xFF2C3030),
+          grayscale400: const Color(0xFF3B3F40),
+          grayscale500: const Color(0xFF808989),
+          grayscale600: const Color(0xFFB4B9B9),
+          grayscale700: const Color(0xFFDADDDD),
+          grayscale800: const Color(0xFFEAEBEB),
+          grayscale900: const Color(0xFFF4F5F5),
+          grayscale1000: const Color(0xFFFDFEFE),
+          primaryBrand: const Color(0xFF2EA4AB),
+          primaryNegative: const Color(0xFFD76263),
+        );
 }
 
 class DPDarkThemeTypography extends DPTypography {
